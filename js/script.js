ea1fa9071a87f8fd41e019ca38549720fae95839
mrizqi130 <muhammadrizqia@student.telkomuniversity.ac.id>
2024-10-09 19:45:06 +0800

    there is some bug when try to clear text area, so we changed textarea to input

--- js/script.js
+++ js/script.js
@@ -3,29 +3,32 @@ function konversi() {
     if (state) {
         let fahren = parseInt(document.getElementById("suhu2").value);
         let celsi = (fahren - 32) * 5 / 9;
-        console.log(celsi);
-        document.getElementById("suhu1").innerHTML = celsi.toFixed(2);
+        // console.log(celsi);
+        document.getElementById("suhu1").value = celsi.toFixed(2);
         document.getElementById("kalkulasi").value = "(" + fahren + "\u00B0F - 32) * 5 / 9 = " + celsi.toFixed(2) + "\u00B0C";
+        console.log("gg")
     } else {
         let celsi = parseInt(document.getElementById("suhu1").value);
         let fahren = celsi * (9 / 5) + 32;
-        console.log(celsi);
-        document.getElementById("suhu2").innerHTML = fahren.toFixed(2);
+        // console.log(celsi);
+        document.getElementById("suhu2").value = fahren.toFixed(2);
         document.getElementById("kalkulasi").value = celsi + "\u00B0C * ( 9 / 5) + 32 = " + fahren.toFixed(2) + "\u00B0F";
+        console.log("ggw")
     }
 }
 function reset_() {
     // location.reload()
     // console.log()
-    var textarea = document.getElementsByTagName("textarea");
+    var input = document.getElementsByClassName("box");
     // console.log(textarea.length)
-    for (var i = 0; i < textarea.length; i++) {
+    for (var i = 0; i < input.length; i++) {
         // console.log()
-        textarea[i].value = ""
+        input[i].value = ""
     }
 }
 function reverse() {
     // alert("reverse");
+    reset_()
     if (state) {
         let fahren = document.getElementById("div1").innerHTML
         let celsi = document.getElementById("div2").innerHTML
