dc080b3c84fb317220a31e705227d8958d9a5a2c
mrizqi130 <muhammadrizqia@student.telkomuniversity.ac.id>
2024-10-10 21:37:51 +0800

    add semantic, css media, function

--- js/script.js
+++ js/script.js
@@ -16,6 +16,39 @@ function konversi() {
         console.log("ggw")
     }
 }
+
+function toCelcius() {
+    console.log("tocelsi")
+    let details = `<h2>Cara Konversi Dari Fahrenheit (&degF) ke Celcius (&degC)</h2>\n
+<hr>
+<p>Suhu S dalam derajat Celcius (&degC) sama dengan suhu S dalam derajat Fahrenheit (&degF) kurang 32 kali 9/5.</p>
+<p>S<sub>(&degC)</sub>=(S<sub>(&degF)</sub>- 32) x 5/9 <br> atau <br> S<sub>(&degC)</sub>=(S<sub>(&degF)</sub>- 32) x 1.8 </p>`
+    let celsi = document.getElementById("div1").innerHTML
+    let fahren = document.getElementById("div2").innerHTML
+    document.getElementById("div1").innerHTML = fahren;
+    document.getElementById("div2").innerHTML = celsi;
+    document.getElementById("suhu1").disabled = true;
+    document.getElementById("suhu2").disabled = false;
+    document.getElementById("details").innerHTML = details;
+}
+
+function toFahrenheit() {
+    console.log("tofahren")
+    let details = `<h2>Cara Konversi Dari Celcius (&degC) ke Fahrenheit (&degF)</h2>\n
+<hr>
+<p>Suhu S dalam derajat Fahrenheit (&degF) sama dengan suhu S dalam derajat Celsius (&degC) kali 9/5 tambah
+    32.</p>
+<p>S<sub>(&degF)</sub>=(S<sub>(&degC)</sub>x 9/5) + 32 <br> atau <br> S<sub>(&degF)</sub>=(S<sub>(&degC)</sub>x 1.8) + 32 </p>`
+    let fahren = document.getElementById("div1").innerHTML
+    let celsi = document.getElementById("div2").innerHTML
+    // fahren to celsi
+    document.getElementById("div1").innerHTML = celsi;
+    document.getElementById("div2").innerHTML = fahren;
+    document.getElementById("suhu1").disabled = false;
+    document.getElementById("suhu2").disabled = true;
+    document.getElementById("details").innerHTML = details;
+
+}
 function reset_() {
     // location.reload()
     // console.log()
@@ -28,24 +61,13 @@ function reset_() {
 }
 function reverse() {
     // alert("reverse");
+    console.log(state)
     reset_()
     if (state) {
-        let fahren = document.getElementById("div1").innerHTML
-        let celsi = document.getElementById("div2").innerHTML
-        // fahren to celsi
-        document.getElementById("div1").innerHTML = celsi;
-        document.getElementById("div2").innerHTML = fahren;
-        document.getElementById("suhu1").disabled = false;
-        document.getElementById("suhu2").disabled = true;
+        toFahrenheit();
         state = false;
     } else {
-        // celsi to fahren
-        let celsi = document.getElementById("div1").innerHTML
-        let fahren = document.getElementById("div2").innerHTML
-        document.getElementById("div1").innerHTML = fahren;
-        document.getElementById("div2").innerHTML = celsi;
-        document.getElementById("suhu1").disabled = true;
-        document.getElementById("suhu2").disabled = false;
+        toCelcius();
         state = true;
     }
 }   
